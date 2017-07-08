3e3eea9e84d897d1e29276cb33884665fbe55040
Cheney Lin <lincheney@gmail.com>
2017-07-09 00:56:51 +1000

    simply tests, dodgy code

--- find-cmd/src/main.rs
+++ find-cmd/src/main.rs
@@ -1,3 +1,20 @@
+pub struct Match {
+    pub start: usize,
+    pub end: usize,
+    pub found: bool,
+}
+
+fn parse_line(line: &str, point: usize) -> Match {
+    let i = 0;
+    let start: Option<usize> = None;
+
+    let start = Some(0);
+    let i = line.len();
+
+    let start = start.unwrap_or(i);
+    Match{start: start, end: i, found: 0 < point && point <= i}
+}
+
 fn main() {
     println!("Hello, world!");
 }
@@ -6,13 +23,20 @@ fn main() {
 mod test {
     use super::parse_line;
 
-    fn assert_parse_line(line: &str, point: usize, expected: &str) {
-        let cmd = parse_line(line, point);
-        assert_eq!( &line[cmd.start..cmd.end], expected );
+    fn assert_parse_line(line: &str, expected: &str) {
+        let point = line.find('_').unwrap();
+        let (left, right) = line.split_at(point);
+        let line = String::new() + left + &right[1..];
+
+        let m = parse_line(&line, point);
+        assert_eq!( &line[m.start..m.end], expected );
     }
 
     #[test]
     fn test_parse_line() {
-        assert_parse_line("echo 123", 1, "echo 123");
+        assert_parse_line("echo_ 123", "echo 123");
+        assert_parse_line(" echo_ 123", "echo 123");
+        assert_parse_line("if echo_ 123", "echo 123");
+        assert_parse_line("when echo_ 123", "echo 123");
     }
 }

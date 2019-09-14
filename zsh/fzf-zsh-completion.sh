77c1553836dcb2f0fd22636cc6c7e27718a66dbf
林千里 <lincheney@gmail.com>
2019-09-14 23:59:47 +1000

    fix printing stderr when no matches

--- zsh/fzf-zsh-completion.sh
+++ zsh/fzf-zsh-completion.sh
@@ -80,6 +80,8 @@ fzf_completion() {
             eval "${(j.;.)__compadd_args:-true} --"
             if (( ! ${#__compadd_args[@]} )) && zstyle -s :completion:::::warnings format msg; then
                 compadd -x "$msg"
+                compadd -x "$stderr"
+                stderr=
             fi
             ;;
     esac

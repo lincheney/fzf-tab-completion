322df2028b76c7d3ad590dd64408256487f4b1ac
林千里 <lincheney@gmail.com>
2023-06-26 20:32:54 +1000

    node: support node inspect, ts-node etc

--- node/fzf-node-completion.js
+++ node/fzf-node-completion.js
@@ -3,31 +3,32 @@
 const child_process = require('node:child_process');
 const repl = require('node:repl');
 
-process.stdin.on('keypress', function(str, key) {
-    if (!repl.repl) {
-        return;
-    }
+const start = repl.start;
+repl.start = function(...args) {
+    const replServer = start(...args);
+    replServer.input.on('keypress', function(str, key) {
+        if (key.sequence == '\t') {
+            // sabotage the key so the repl doesn't get it
+            key.name = '';
+            key.ctrl = 1;
+            key.sequence = '';
 
-    if (key.sequence == '\t') {
-        // sabotage the key so the repl doesn't get it
-        key.name = '';
-        key.ctrl = 1;
-        key.sequence = '';
-
-        repl.repl.completer(repl.repl.line.slice(0, repl.repl.cursor), function(error, [completions, prefix]) {
-            if (completions.length == 0) {
-                return;
-            }
-            let stdout = prefix;
-            const input = completions.filter(x => x !== '').join('\n');
-            try {
-                stdout = child_process.execFileSync('rl_custom_complete', [prefix], {input, stdio: ['pipe', 'pipe', 'inherit']}).toString().trim('\n');
-            } catch(e) {
-            }
-            repl.repl.line = repl.repl.line.slice(0, repl.repl.cursor - prefix.length) + stdout + repl.repl.line.slice(repl.repl.cursor);
-            repl.repl.cursor += stdout.length - prefix.length;
-            // fzf will have destroyed the prompt, so fix it
-            repl.repl._refreshLine();
-        });
-    }
-})
+            replServer.completer(replServer.line.slice(0, replServer.cursor), function(error, [completions, prefix]) {
+                if (completions.length == 0) {
+                    return;
+                }
+                let stdout = prefix;
+                const input = completions.filter(x => x !== '').join('\n');
+                try {
+                    stdout = child_process.execFileSync('rl_custom_complete', [prefix], {input, stdio: ['pipe', 'pipe', 'inherit']}).toString().trim('\n');
+                } catch(e) {
+                }
+                replServer.line = replServer.line.slice(0, replServer.cursor - prefix.length) + stdout + replServer.line.slice(replServer.cursor);
+                replServer.cursor += stdout.length - prefix.length;
+                // fzf will have destroyed the prompt, so fix it
+                replServer._refreshLine();
+            });
+        }
+    });
+    return replServer;
+};
